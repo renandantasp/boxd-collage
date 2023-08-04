4c5aced65b64f9795902018119a2d4d8ab4e8f24
Renan <renandantasp@gmail.com>
2023-08-03 21:57:36 -0300

    refactor collage.ts

--- movie-collage/src/pages/api/collage.ts
+++ movie-collage/src/pages/api/collage.ts
@@ -1,60 +1,70 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { createCanvas, loadImage } from 'canvas'
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { createCanvas, loadImage } from 'canvas';
+import fetch from 'node-fetch';
+
+const width = 200;
+const height = width * 1.5;
+const font_size = 10;
+
+async function loadImageAsync(url) {
+  const image = await loadImage(url);
+  return image;
+}
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  
-  const mainUrl = (process.env.enviroment === "production" ? process.env.productionUrl : process.env.devUrl)
+  const mainUrl =
+    process.env.enviroment === 'production'
+      ? process.env.productionUrl
+      : process.env.devUrl;
 
-  const response = await fetch(`${mainUrl}api/film_scraper?user=${req.query.user}&time=${req.query.time}`)
-  const collage_films = await response.json()
+  const response = await fetch(
+    `${mainUrl}api/film_scraper?user=${req.query.user}&time=${req.query.time}`
+  );
+  const collage_films = await response.json();
 
-  if (collage_films.length == 0) {
-    res.status(500).send({ error: 'No films to display' });
+  if (collage_films.length === 0) {
+    return res.status(500).send({ error: 'No films to display' });
   }
 
-  const size = Number(req.query.size)
-  const films = (collage_films.length > size*size) ? collage_films.slice(0,size*size) : collage_films
-  const width = 150
-  const height = width*1.5
-  const font_size = 10
+  const size = Number(req.query.size);
+  const films = collage_films.slice(0, size * size);
 
-  const canvas = createCanvas(width*size, height*size)
-  const ctx = canvas.getContext('2d')
-  ctx.fillStyle = "black"
-  ctx.fillRect(0,0,width*size,height*size)
-  ctx.fillStyle = "white"
-  ctx.font = "12px Courier New";
+  const canvas = createCanvas(width * size, height * size);
+  const ctx = canvas.getContext('2d');
+
+  ctx.fillStyle = 'black';
+  ctx.fillRect(0, 0, width * size, height * size);
+  ctx.fillStyle = 'white';
+  ctx.font = '12px Courier New';
   ctx.lineWidth = 2;
-  
-  let film_index = 0
-  
-  for (let i=0; i < size; i++){
-    for (let j=0; j< size && film_index < films.length; j++){
-      const image = await loadImage(films[film_index].poster_path)
-      ctx.drawImage(image, width*j, height*i, width, height)
-      
-      const grd = ctx.createLinearGradient(0, i*height, 0, (i*height)+height)
-      grd.addColorStop(0, "#00000070");
-      grd.addColorStop(0.25, "transparent");
-      grd.addColorStop(1, "transparent");
 
-      ctx.fillStyle = grd;
-      ctx.fillRect(width*j, height*i, width, height);
+  const filmPromises = films.map(async (film, index) => {
+    const image = await loadImageAsync(film.poster_path);
+    const i = Math.floor(index / size);
+    const j = index % size;
 
-      ctx.fillStyle = 'white';
-      ctx.fillText(films[film_index].film_name, (width*j)+5, (height*i)+font_size+5,width)
-      
-      ctx.fillStyle = "#00e054"
-      ctx.fillText(films[film_index].rating, (width*j)-4, (height*i)+(font_size*2.25)+5,width)
-      
-      film_index = film_index + 1
-    }
-  }
+    ctx.drawImage(image, width * j, height * i, width, height);
 
-  const dataURL = canvas.toDataURL('image/jpeg');
-  res.status(200).send({src:dataURL});
+    const grd = ctx.createLinearGradient(0, i * height, 0, i * height + height);
+    grd.addColorStop(0, '#00000070');
+    grd.addColorStop(0.25, 'transparent');
+    grd.addColorStop(1, 'transparent');
+
+    ctx.fillStyle = grd;
+    ctx.fillRect(width * j, height * i, width, height);
+
+    ctx.fillStyle = 'white';
+    ctx.fillText(film.film_name, width * j + 5, height * i + font_size + 5, width);
+
+    ctx.fillStyle = '#00e054';
+    ctx.fillText(film.rating, width * j - 4, height * i + font_size * 2.25 + 5, width);
+  });
+
+  await Promise.all(filmPromises);
+
+  const dataURL = canvas.toDataURL('image/png');
+  res.status(200).send({ src: dataURL });
 }
